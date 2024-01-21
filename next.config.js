d22e9cffe91c9af638f1006ca8af4f33279a9ea1
Laurent <o.sherkes@gmail.com>
2024-01-21 13:53:50 +0100

    fixes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['https://nextjs-dashboard-7sno.onrender.com']
+    }
   }
 };
 
