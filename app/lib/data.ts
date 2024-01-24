ad4daaeff8491af4cb9059b7208310010920c516
Saint Laurent <stlaurentcodes@gmail.com>
2024-01-24 13:09:50 +0100

    fix: fix more types

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -23,7 +23,7 @@ export async function fetchRevenue() {
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await client.$queryRaw<Revenue>`SELECT * FROM revenue`;
+    const data = await client.$queryRaw<Revenue[]>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
 
